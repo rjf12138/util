54796f30110a4657ed84ac202d6b318dbc889fc2
rjf12138 <3161173206@qq.com>
2022-05-23 22:00:18 +0800

    2022年 05月 23日 星期一 22:00:18 CST

---
+++ build/CMakeFiles/util.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ruanjian/workspace/project_v/util/src/msg_object.cc" "CMakeFiles/util.dir/src/msg_object.cc.o" "gcc" "CMakeFiles/util.dir/src/msg_object.cc.o.d"
+  "/home/ruanjian/workspace/project_v/util/src/timer.cc" "CMakeFiles/util.dir/src/timer.cc.o" "gcc" "CMakeFiles/util.dir/src/timer.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/util.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/util.dir/src/msg_object.cc.o"
+  "CMakeFiles/util.dir/src/msg_object.cc.o.d"
+  "CMakeFiles/util.dir/src/timer.cc.o"
+  "CMakeFiles/util.dir/src/timer.cc.o.d"
+  "libutild.a"
+  "libutild.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/util.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
